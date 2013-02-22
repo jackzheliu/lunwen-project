e51504e06c339bb8c6a53cab0af048a263dca0f1
jack <jack@healthywebsites.co.uk>
2013-02-22 11:43:43 +0000

    write to log file

--- app/code/local/Sms/Receivesms/controllers/SmsController.php
+++ app/code/local/Sms/Receivesms/controllers/SmsController.php
@@ -3,6 +3,15 @@
 class Sms_Receivesms_SmsController extends Mage_Core_Controller_Front_Action
 {
     public function smsAction(){
-     echo "Mate Go fuck urself";
+
+     $postData = Mage::app()->getRequest()->getPost();
+
+     if (isset($postData['From'])) {
+
+     	Mage::log(isset($postData['From']));
+
+	 }
+
+
     }
 }
