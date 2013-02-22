efb5260599149097034bf0c31f7458038ac213b9
root <root@ip-10-48-234-148.eu-west-1.compute.internal>
2013-02-22 13:00:24 +0000

    test working

--- app/code/local/Sms/Receivesms/controllers/SmsController.php
+++ app/code/local/Sms/Receivesms/controllers/SmsController.php
@@ -4,13 +4,17 @@ class Sms_Receivesms_SmsController extends Mage_Core_Controller_Front_Action
 {
     public function smsAction(){
 
-     $postData = Mage::app()->getRequest()->getPost();
+    // $postData = Mage::app()->getRequest()->getPost();
 
-     if (isset($postData['From'])) {
+    // if (isset($postData['From'])) {
 
-     	Mage::log(isset($postData['From']));
+     //	Mage::log('Mate go fuck youself'.isset($postData['SmsSid']));
 
-	 }
+//	 }
+
+	$phonenumber = Mage::app()->getRequest()->getParam('From');
+
+        Mage::log('Mate go fuck youself111'.$phonenumber);
 
 
     }
